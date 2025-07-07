5d877b007f8268c229ba998610f402cb0954b74a
Ali Raza <mrarcoder@gmail.com>
2025-07-07 11:39:54 +0500

    learned If else

---
+++ 3. Control Structures/main.go
@@ -0,0 +1,20 @@
+// 3. Control Structures
+package main
+
+import "fmt"
+
+func main() {
+	// If-else statement
+	fmt.Print("> What is your age?\n> ")
+	age := 0
+	fmt.Scanln(&age)
+	if age < 18 && age > 0 {
+		fmt.Println("> Teenager")
+	} else if age >= 18 && age <= 64 {
+		fmt.Println("> Adult")
+	} else if age >= 65 {
+		fmt.Println("> Senior")
+	} else {
+		fmt.Println("> Invalid Input")
+	}
+}
